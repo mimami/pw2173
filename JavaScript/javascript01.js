9ba9e2e3bc0e8d5ddc5cdd0f4f2919de0df34c4b
mimami <pacoalvi1@hotmail.com>
2017-11-06 15:58:10 -0700

    Introducción a JavaScript

---
+++ JavaScript/javascript01.js
@@ -0,0 +1,9 @@
+var datos = function()
+    {
+		var nombre;
+		var edad;
+		nombre = prompt("Teclea tu nombre");
+		edad = prompt("Teclea tu edad");
+	    document.write("Hola " + nombre);
+	    document.write(" tienes " + edad + " años");
+	}
