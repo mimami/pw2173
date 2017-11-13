65d0d89cfc29f867e5dbffac1c0818d67bb90bdd
mimami <pacoalvi1@hotmail.com>
2017-11-13 15:54:40 -0700

    JQuery

--- jquery/js/eventosjquery.js
+++ jquery/js/eventosjquery.js
@@ -10,14 +10,20 @@ var inicia = function(){
     //console.log(data);
     
     //el método html sirve para incrustar texto o código
-    $("#nombre").html("<h1>"+data.results[0].name.first +" "+ data.results[0].name.last);
+    $("#nombre").html("<b>Nombre: </b>"+data.results[0].name.first +" "+ data.results[0].name.last);
+    $("#genero").html("<b>Género: </b>"+data.results[0].gender);
     //el método attr cambia el contenido del atributo, sirve para obtener y/o 
     //modificar valores en los atributos de los diferentes elementos
 
     //$("#nombre").append(data.results[0].name.first +" "+ data.results[0].name.last);
     //el método append sirve para agregar contenido al html sin que este se pierda 
     $("#foto").attr("src",data.results[0].picture.large); 
-    $("#direccion").html("Dirección: "+data.results[0].location.city);
+    $("#direccion").html("<b>Dirección: </b>"+data.results[0].location.street);
+    $("#ciudad").html("<b>Ciudad: </b>"+data.results[0].location.city);
+    $("#estado").html("<b>Estado: </b>"+data.results[0].location.state);
+    $("#codigopostal").html("<b>C.P: </b>"+data.results[0].location.postcode);
+    $("#telefono").html("<b>Teléfono: </b>"+data.results[0].phone);
+    $("#celular").html("<b>Celular: </b>"+data.results[0].cell);
   
 
   	}
