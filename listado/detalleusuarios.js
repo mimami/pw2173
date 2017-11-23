eb93e43810e0c09bfcca07621fe89b769926073b
mimami <pacoalvi1@hotmail.com>
2017-11-23 16:10:32 -0700

    Listado electron

---
+++ listado/detalleusuarios.js
@@ -0,0 +1,24 @@
+const {BrowserWindow}=require('electron').remote
+const app=require('electron').app;
+const $ = require('jquery');
+
+//Constantes para llamar al PDF
+const ipc=require('electron').ipcRenderer
+const botonPDF=document.getElementById('btnPDF')
+//Activamos el evento click del botón btnPDF
+botonPDF.addEventListener('click',function(event){
+	botonPDF.style.display="none" //Desaparece el botón antes de que imprima en PDF
+	ipc.send('print-to-pdf')
+})
+
+var nombre=require('electron').remote.getGlobal('infoUsuarios').nombre;
+var genero=require('electron').remote.getGlobal('infoUsuarios').genero;
+var foto=require('electron').remote.getGlobal('infoUsuarios').foto;
+var direccion=require('electron').remote.getGlobal('infoUsuarios').direccion;
+var telefono=require('electron').remote.getGlobal('infoUsuarios').telefono;
+
+$("#idNombre").html(nombre);
+$("#idGenero").html(genero);
+$("#idDireccion").html(direccion);
+$("#idTelefono").html(telefono);
+$("#idFoto").attr("src",foto);
--- listado/main.js
+++ listado/main.js
@@ -4,10 +4,28 @@
 const {app,BrowserWindow} = require('electron'); //Lo que está en el conjunto, búscalo como lo que está del lado derecho
 const url = require('url'); //Carga una página
 const path = require('path'); //Muestra la ruta del archivo y este carga una página
+
+//Constantes para PDF
+const electron=require('electron');
+const fs=require('fs');//sistema de archivos de la computadora
+//acceso al sistema operativo
+const os=require('os');
+//Para declarar una funcion remota
+const ipc=electron.ipcMain;
+//Acceso a la terminal-linea de comandos
+const shell=electron.shell;
 //ECMASCRIPT = 6
 let PantallaPrincipal; 
 //let es una constante que tendrá el valor mas adelante, para cuando
 //lo tenga, ya no podrá cambiar su valor
+
+global.infoUsuarios={
+	nombre: '',
+	genero: '',
+	foto: '',
+	direccion: '',
+	telefono: ''
+}
 
 function muestraPantallaPrincipal(){
 	PantallaPrincipal = new BrowserWindow({width:320,height:425});
@@ -19,6 +37,23 @@ function muestraPantallaPrincipal(){
 	//PantallaPrincipal.webContents.openDevTools(); 
 	PantallaPrincipal.show(); //muestra la pantalla
 }
+
+//evento para PDF (declaracion)
+ipc.on('print-to-pdf',function(event){
+	const pdfPath=path.join(os.tmpdir(),'print.pdf') //a la ruta la une con una carpeta temporal y el nombre del archivo
+	const win=BrowserWindow.fromWebContents(event.sender)
+	win.webContents.printToPDF({},function(error,data){
+		if(error) throw error
+		fs.writeFile(pdfPath,data,function(error){
+		if(error){ //Si hay error, detiene el programa
+			throw error 
+		}
+		shell.openExternal('file://' + pdfPath) //Accesa al archivo en pdf
+		win.close(); //Cierra la ventana que se está procesando
+		
+		})
+	})
+});
 
 
 app.on('ready',muestraPantallaPrincipal) //encender la aplicación, y cuando esté listo usa la función muestraPantallaPrincipal
