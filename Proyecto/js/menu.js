35db6d3d98748457b7a7d49bba49aa6111752a12
mimami <pacoalvi1@hotmail.com>
2017-12-07 15:57:12 -0700

    Altaalumnos PHP (continuacion)

--- Proyecto/js/menu.js
+++ Proyecto/js/menu.js
@@ -31,6 +31,32 @@ var iniciaMenu = function(){
 			 }
 		});
 	}
+	var teclaNcontrol=function(tecla){
+		if(tecla.which == 13) //13=Enter
+		{
+			var ncontrol=$("txtNcontrol").val();
+			var parametros="opc=buscaNcontrol"+
+							"&ncontrol="+ncontrol+
+							"&id="+Math.random();
+			$.ajax({
+			 url:"php/entrada.php",
+			 dataType: 'json', //retorno
+			 type: "POST", //lo que enviamos
+			 data:parametros,
+			 success:function(data){
+			 	if(data.respuesta == true){
+			 		//alert("Bienvenido"+data.nombre);
+			 		$("main").load("menu.html");
+			 	}else{
+			 		alert("Usuario y/o clave incorrectos");
+			 	}
+			 },
+			 error:function(a,b,c){
+			 	alert("No se pudo conectar al server");
+			 }
+		});
+		}
+	}
 
 	}
 
